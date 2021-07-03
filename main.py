85dd4558de562ee828ef67ea05d1b89618433291
EnacRobotique <robotique.enac@gmail.com>
2021-07-03 15:47:40 +0200

    updated code

--- main.py
+++ main.py
@@ -12,21 +12,26 @@ class OutilDiagnostic(QWidget):
         self.resize(600, 700)
         self.listen = True 
         self.zoom = zoom
-        self.serialObject = serial.Serial (port = nom_port, baudrate=115200, timeout =1)
+        self.serialObject = serial.Serial (port = nom_port, baudrate=57600, timeout =1)
         self.thread_ecoute = threading.Thread ( target=self.ecoute,)
         self.width = self.frameGeometry().width()
         self.height = self.frameGeometry().height()
         self.dist_dict = [0 for _ in range(360)]
-        self.valid_dict = [0 for _ in range(360)]
-        self.inzone_dict = [0 for _ in range(360)]
-        self.true_pos = [[0, 0] for _ in range(360)]
+        self.quality_dict = [0 for _ in range(360)]
         self.motorSpeed = 0
         self.timer = QTimer()
         self.timer.timeout.connect(self.repaint)
         self.timer.start(150)
+        self.reset_timer = QTimer()
+        self.reset_timer.timeout.connect(self.reset_dicts)
+        self.reset_timer.start(3000)
 
     def closeEvent(self, event):
         self.stop()
+
+    def reset_dicts(self):
+        self.dist_dict = [0 for _ in range(360)]
+        self.quality_dict = [0 for _ in range(360)]
 
     def paintEvent(self, event):
         """Evt appellé à chaque fois que le widget est resize ou caché"""
@@ -48,24 +53,17 @@ class OutilDiagnostic(QWidget):
         painter.drawText(QPoint(30, 30), "MotorSpeed: {}".format(self.motorSpeed))
         painter.drawText(QPoint(30, 40), "0° vers le haut, 90° vers la gauche")
         for angle, dist in enumerate(self.dist_dict):
-            c = int(self.width/2 - dist/4*self.zoom*math.cos((angle - 90)/180*math.pi))
+            c = int(self.width/2 + dist/4*self.zoom*math.cos((angle - 90)/180*math.pi))
             d = int(self.height/2 + dist/4*self.zoom*math.sin((angle - 90)/180*math.pi))
-            if self.valid_dict[angle]:
-                if self.inzone_dict[angle]:
-                    painter.setPen(QPen(Qt.blue, 3))
-                else:
-                    painter.setPen(QPen(Qt.red, 3))
-                painter.drawPoint(QPoint(c, d))
-            else:
-                painter.setPen(QPen(QColor(0, 0, 0, alpha=100), 3))
-                c = int(self.width/2 - 1000/4*self.zoom*math.cos((angle - 90)/180*math.pi))
-                d = int(self.height/2 + 1000/4*self.zoom*math.sin((angle - 90)/180*math.pi))
+            if self.quality_dict[angle] == 0:
+                painter.setPen(QPen(QColor(200, 0, 0, alpha=5), 3))
                 painter.drawLine(QPoint(a, b), QPoint(c, d))
-            if angle%8 == 0 and self.valid_dict[angle]:
-                painter.drawText(QPoint(c, d), "{}: {}, {}".format(
-                    angle, self.true_pos[angle][0], self.true_pos[angle][1]))
-                painter.drawText(QPoint(c, d + 10), "{}, {}".format(
-                    int(1500 + dist*math.cos(angle/180*math.pi)), int(1000 + dist*math.sin(angle/180*math.pi))))
+            else:
+                if dist > 3000:
+                    painter.setPen(QPen(QColor(0, 0, 200, alpha=30), 3))
+                else:
+                    painter.setPen(QPen(QColor(0, 150, 0, alpha=30+self.quality_dict[angle]*3), 3))
+                painter.drawLine(QPoint(a, b), QPoint(c, d))
         
     #Autres méthodes très utiles
 
@@ -75,12 +73,19 @@ class OutilDiagnostic(QWidget):
             message = message.decode ()
             if len (message) != 0:
                 msg_s = message.split()
-                print(msg_s)
-                if len(msg_s) == 7:
-                    angle = int(msg_s[0])
-                    if angle < 360:
-                        self.dist_dict[angle] = int(msg_s[1])
-                        self.quality_dict[angle] = int(msg_s[2])
+                #print(msg_s)
+                if len(msg_s) >= 3:
+                    try:
+                        angle = int(float((msg_s[0])))
+                        if angle < 360:
+                            self.dist_dict[angle] = int(float(msg_s[1]))
+                            self.quality_dict[angle] = int(float(msg_s[2]))
+                            if len(msg_s) == 3:
+                                print(angle, self.dist_dict[angle], self.quality_dict[angle])
+                            else:
+                                print(angle, self.dist_dict[angle], self.quality_dict[angle], msg_s[3])
+                    except:
+                        pass
 
     def start (self):
         """Démarre le thread d'écoute"""
